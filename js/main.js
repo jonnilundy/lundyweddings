3847f197caf48c15b3af1f63ae738e0638fe911f
Jonathan Lundy <jonathan.lundy@liferay.com>
2016-11-29 11:07:57 -0800

    fix broken js connection

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ form.addEventListener('submit', function(e) {
 	e.preventDefault();
 
 	WeDeploy
-		.url('http://email.boilerplate-email.wedeploy.io/emails')
+		.url('http://contact.lundyweddings.wedeploy.io/emails')
 		.auth('49488085-1ee3-4c33-888f-0a1e37d2694e')
-		.form('from', form.from.value)
+		.form('from', 'hello@lundyweddings.com')
 		.form('to', form.to.value)
-		.form('subject', form.subject.value)
-		.form('message', form.subject.value)
+		.form('subject', form.message.value)
+		.form('message', form.message.value)
 		.post()
 		.then(function(response) {
 			if (response.succeeded()) {
